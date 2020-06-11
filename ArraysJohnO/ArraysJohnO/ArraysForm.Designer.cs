2a55e15b7c0dc2d049919cdbce143c9b5d0dcc8f
John Omage <47784108+s422862@users.noreply.github.com>
2020-06-10 22:48:45 -0400

    Add files via upload

---
+++ ArraysJohnO/ArraysJohnO/ArraysForm.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace ArraysJohnO
+{
+    partial class frmArrayAverage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lstNumbers = new System.Windows.Forms.ListBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(151, 70);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(91, 28);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lstNumbers
+            // 
+            this.lstNumbers.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstNumbers.FormattingEnabled = true;
+            this.lstNumbers.ItemHeight = 20;
+            this.lstNumbers.Location = new System.Drawing.Point(72, 117);
+            this.lstNumbers.Name = "lstNumbers";
+            this.lstNumbers.Size = new System.Drawing.Size(274, 204);
+            this.lstNumbers.TabIndex = 2;
+            this.lstNumbers.SelectedIndexChanged += new System.EventHandler(this.lstNumbers_SelectedIndexChanged);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(46, 372);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(123, 56);
+            this.btnCalculate.TabIndex = 3;
+            this.btnCalculate.Text = "Calculate Average";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnswer.Location = new System.Drawing.Point(249, 390);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(51, 20);
+            this.lblAnswer.TabIndex = 4;
+            this.lblAnswer.Text = "label1";
+            this.lblAnswer.Click += new System.EventHandler(this.lblAnswer_Click);
+            // 
+            // frmArrayAverage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(443, 450);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lstNumbers);
+            this.Controls.Add(this.btnStart);
+            this.Name = "frmArrayAverage";
+            this.Text = "Array Average";
+            this.Load += new System.EventHandler(this.frmArrayAverage_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ListBox lstNumbers;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblAnswer;
+    }
+}
+
---
+++ ArraysJohnO/ArraysJohnO/ArraysForm.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArraysJohnO
+{
+    public partial class frmArrayAverage : Form
+    {
+        // declare global variables and constants
+        const int MAX_ARRAY_SIZE = 10;
+        int[] arrayOfNumbers = new int[MAX_ARRAY_SIZE];
+
+        public frmArrayAverage()
+        {
+            InitializeComponent();
+            // hide label
+            lblAnswer.Hide();
+
+            // disable buttons
+            btnCalculate.Enabled = false;
+        }
+
+        private void frmArrayAverage_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            // declare local variables
+            int randomNumber;
+            int counter;
+            Random randomNumberGenerator = new Random();
+
+            // Enable buttons
+            btnCalculate.Enabled = true;
+
+            // Clear list box
+            this.lstNumbers.Items.Clear();
+
+            for (counter = 0; counter < MAX_ARRAY_SIZE; counter++)
+            {
+                // generate a new random number between 1 and the max array size
+                randomNumber = randomNumberGenerator.Next(1, MAX_ARRAY_SIZE + 1);
+
+                // assign the random number to the cell at position counter in the array
+                arrayOfNumbers[counter] = randomNumber;
+
+                // Display list
+                this.lstNumbers.Items.Add(randomNumber);
+
+                // refresh the form to display the new item in the list box
+                this.Refresh();
+            }
+        }
+
+        private void lstNumbers_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            // declare local variables
+            double average = 0;
+            double sum = 0;
+            int counter;
+
+            // Show label
+            lblAnswer.Show();
+
+            // calculate the sum of all the values in the array 
+            for (counter = 0; counter < arrayOfNumbers.Length; counter++)
+            {
+                sum = sum + arrayOfNumbers[counter];
+            }
+
+            // calculate the average of the values in the array 
+            average = sum / arrayOfNumbers.Length;
+
+            // display the average in the label
+            lblAnswer.Text = "Average: " + average;
+        }
+
+        private void lblAnswer_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
