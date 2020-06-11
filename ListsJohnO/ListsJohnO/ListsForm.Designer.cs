0bb4d02cbf922ca6b60cc591648885c4c2d3f31d
John Omage <47784108+s422862@users.noreply.github.com>
2020-06-10 23:22:31 -0400

    Add files via upload

---
+++ ListsJohnO/ListsJohnO/ListsForm.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace ListsJohnO
+{
+    partial class frmLists
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMark = new System.Windows.Forms.Label();
+            this.txtMark = new System.Windows.Forms.TextBox();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.lstNumbers = new System.Windows.Forms.ListBox();
+            this.btnAverage = new System.Windows.Forms.Button();
+            this.lblAverage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblMark
+            // 
+            this.lblMark.AutoSize = true;
+            this.lblMark.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMark.Location = new System.Drawing.Point(12, 47);
+            this.lblMark.Name = "lblMark";
+            this.lblMark.Size = new System.Drawing.Size(200, 16);
+            this.lblMark.TabIndex = 1;
+            this.lblMark.Text = "Enter a mark between 0 and 100:";
+            this.lblMark.Click += new System.EventHandler(this.lblMark_Click);
+            // 
+            // txtMark
+            // 
+            this.txtMark.Location = new System.Drawing.Point(227, 46);
+            this.txtMark.Name = "txtMark";
+            this.txtMark.Size = new System.Drawing.Size(100, 20);
+            this.txtMark.TabIndex = 2;
+            this.txtMark.TextChanged += new System.EventHandler(this.txtMark_TextChanged);
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Location = new System.Drawing.Point(137, 86);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(75, 23);
+            this.btnEnter.TabIndex = 3;
+            this.btnEnter.Text = "Enter";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // lstNumbers
+            // 
+            this.lstNumbers.FormattingEnabled = true;
+            this.lstNumbers.Location = new System.Drawing.Point(36, 124);
+            this.lstNumbers.Name = "lstNumbers";
+            this.lstNumbers.Size = new System.Drawing.Size(280, 238);
+            this.lstNumbers.TabIndex = 4;
+            this.lstNumbers.SelectedIndexChanged += new System.EventHandler(this.lstNumbers_SelectedIndexChanged);
+            // 
+            // btnAverage
+            // 
+            this.btnAverage.Location = new System.Drawing.Point(137, 368);
+            this.btnAverage.Name = "btnAverage";
+            this.btnAverage.Size = new System.Drawing.Size(75, 23);
+            this.btnAverage.TabIndex = 5;
+            this.btnAverage.Text = "Average";
+            this.btnAverage.UseVisualStyleBackColor = true;
+            this.btnAverage.Click += new System.EventHandler(this.btnAverage_Click);
+            // 
+            // lblAverage
+            // 
+            this.lblAverage.AutoSize = true;
+            this.lblAverage.Location = new System.Drawing.Point(46, 415);
+            this.lblAverage.Name = "lblAverage";
+            this.lblAverage.Size = new System.Drawing.Size(81, 13);
+            this.lblAverage.TabIndex = 6;
+            this.lblAverage.Text = "The average is:";
+            this.lblAverage.Click += new System.EventHandler(this.lblAverage_Click);
+            // 
+            // frmLists
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(366, 450);
+            this.Controls.Add(this.lblAverage);
+            this.Controls.Add(this.btnAverage);
+            this.Controls.Add(this.lstNumbers);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.txtMark);
+            this.Controls.Add(this.lblMark);
+            this.Name = "frmLists";
+            this.Text = "Lists";
+            this.Load += new System.EventHandler(this.frmLists_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMark;
+        private System.Windows.Forms.TextBox txtMark;
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.ListBox lstNumbers;
+        private System.Windows.Forms.Button btnAverage;
+        private System.Windows.Forms.Label lblAverage;
+    }
+}
+
